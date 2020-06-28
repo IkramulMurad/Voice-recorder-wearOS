5217247f93fdd8f731404620f2026421539429ec
Ikramul Murad <ikramul.murad1993@gmail.com>
2020-06-28 21:18:41 +0600

    Fix recorder view layout
    Signed-off-by: Ikramul Murad <ikramul.murad1993@gmail.com>

---
+++ Voicerecorder/app/src/main/java/com/example/voice_recorder/CustomChronometer.java
@@ -0,0 +1,45 @@
+package com.example.voice_recorder;
+
+import android.app.Activity;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Chronometer;
+
+public class CustomChronometer {
+    private Chronometer chronometer;
+    private long pauseOffset;
+    private boolean running;
+
+    public CustomChronometer(Activity activity, int resId) {
+        chronometer = (Chronometer)activity.findViewById(resId);
+        chronometer.setFormat("%s");
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                if ((SystemClock.elapsedRealtime() - chronometer.getBase()) >= 3600) {
+                    chronometer.setBase(SystemClock.elapsedRealtime());
+                }
+            }
+        });
+    }
+
+    public void startChronometer(View v) {
+        if (!running) {
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            chronometer.start();
+            running = true;
+        }
+    }
+    public void pauseChronometer(View v) {
+        if (running) {
+            chronometer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+            running = false;
+        }
+    }
+    public void resetChronometer(View v) {
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        pauseOffset = 0;
+    }
+}
--- Voicerecorder/app/src/main/java/com/example/voice_recorder/MainActivity.java
+++ Voicerecorder/app/src/main/java/com/example/voice_recorder/MainActivity.java
@@ -2,20 +2,34 @@ package com.example.voice_recorder;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends WearableActivity {
-
-    private TextView mTextView;
+    private CustomChronometer recordingTimer;
+    private ImageButton cancelRecording;
+    private ImageButton startRecording;
+    private ImageButton stopRecording;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
-
         // Enables Always-on
         setAmbientEnabled();
+
+        initView();
+    }
+
+    private void initView() {
+        recordingTimer = new CustomChronometer(this, R.id.recording_timer);
+
+        startRecording = findViewById(R.id.start_recording);
+        stopRecording = findViewById(R.id.stop_recording);
+        cancelRecording = findViewById(R.id.cancel_recording);
+
+        stopRecording.setVisibility(View.GONE);
+        cancelRecording.setVisibility(View.GONE);
     }
 }
